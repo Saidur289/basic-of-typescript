fd513654bdd47344ddb85fb41f782cb1b548f14d
Saidur289 <saidur.riaz1@gmailcomm>
2025-05-08 14:22:36 +0600

    basic about assign type

--- app.ts
+++ app.ts
@@ -9,4 +9,9 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string){
 }
 const result = true;
 const tilte = 'Result is :'
-add(4, 5, result, tilte)
+// add(4, 5, result, tilte)
+// type assignment and good practice 
+// let number1: number;
+// number1 = 5
+// let title = 'hello world';
+// tilte = 0; give error already assign in string 
